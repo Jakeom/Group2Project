97c8ac901e116763a6266f33b4c26ac21dfb52bd
Harpreet singh <preet4953@gmail.com>
2021-08-02 07:36:26 -0400

    Made the views and attach the classes

---
+++ Group2Project/CreateMemory.swift
@@ -0,0 +1,29 @@
+//
+//  CreateMemory.swift
+//  Group2Project
+//
+//  Created by user196663 on 8/2/21.
+//
+
+import UIKit
+
+class CreateMemory: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
